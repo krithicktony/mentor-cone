ed03e69cf112c9a6ea74dd83d948a7792b4e7df7
krithicktony <toinboxofkrithick@outlook.com>
2024-12-07 02:02:01 -0500

    menter-cone

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function showslider(){
+    const slider = document.querySelector('.navright')
+    slider.style.display = 'flex'
+    event.preventDefault();
+}
+function hideslider(){
+    const slider = document.querySelector('.navright')
+    slider.style.display = 'none'
+    event.preventDefault();
+}
